97a92769d923ade2efdc9d9be9d23a13e68849b4
AnrubioG <angela.rubio@genstudents.org>
2024-05-30 09:07:29 -0500

    html and css are added

---
+++ app.js
@@ -0,0 +1,28 @@
+// ----------------------- Selectores del DOM ----------------------- //
+const from = document.querySelector("#form")
+const select = document.querySelector("#select")
+const input = document.querySelector("#input")
+const submit = document.querySelector("#submit")
+const unitC = document.querySelector("#unitC")
+const unitF = document.querySelector("#unitF")
+const unitK = document.querySelector("#unitK")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -----------------------  ----------------------- //
+// -----------------------  ----------------------- //
+// -----------------------  ----------------------- //
+// -----------------------  ----------------------- //
